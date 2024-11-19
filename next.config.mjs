a569cd5d256c70c20dd91eedbe668134a69d490a
roberto mendoza <robertoemendozac@gmail.com>
2024-11-19 17:13:58 -0400

    add iniciosmyr host to load images

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
       {
         hostname: "s3-inicios-images.s3.us-east-1.amazonaws.com",
         protocol: "https"
+      },
+      {
+        hostname: "system-managment.iniciosmyr.com",
+        protocol: "https"
       }
     ]
   }
